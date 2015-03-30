d58b5a3504334fbf8fadf18e7dad5e60111a4d7d
Geoff <geoffroy.cowan@gmail.com>
2015-03-30 15:20:59 +1100

    Changed back from hello world.

--- srtm_server.py
+++ srtm_server.py
@@ -10,58 +10,58 @@ elevation_data = srtm.get_data()
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-@cross_origin()
-def create_task():
-  return "HELLO WORLD!", 200
-
-# @app.route('/api/getElevations', methods=['GET'])
+# @app.route('/', methods=['GET'])
 # @cross_origin()
 # def create_task():
-#   print "INCOMING!!!!!!--: " + str(datetime.datetime.now())
-#   intime = datetime.datetime.now()
-#   # TODO (gbcowan) check if input is valid
+#   return "HELLO WORLD!", 200
+
+@app.route('/api/getElevations', methods=['GET'])
+@cross_origin()
+def create_task():
+  print "INCOMING!!!!!!--: " + str(datetime.datetime.now())
+  intime = datetime.datetime.now()
+  # TODO (gbcowan) check if input is valid
   
 
 
 
-#   north = float(request.args['north'])
-#   south = float(request.args['south'])
-#   east = float(request.args['east'])
-#   west = float(request.args['west'])
-#   resolution = float(request.args['resolution'])
-#   print north
-#   print south
-#   print east
-#   print west
-#   print resolution
+  north = float(request.args['north'])
+  south = float(request.args['south'])
+  east = float(request.args['east'])
+  west = float(request.args['west'])
+  resolution = float(request.args['resolution'])
+  print north
+  print south
+  print east
+  print west
+  print resolution
 
-#   lon = west
-#   lat = north
-#   x_step_size = math.fabs(east-west) / resolution
-#   y_step_size = math.fabs(north-south) / resolution
-#   points = []
+  lon = west
+  lat = north
+  x_step_size = math.fabs(east-west) / resolution
+  y_step_size = math.fabs(north-south) / resolution
+  points = []
 
-#   while(lat > south):
-#     if (lat <= -90): lat += 180
-#     lon = west
-#     row = []
-#     while(lon < east):
-#       if (lon >= 180): lon -=360
-#       row.append([lat, lon])
-#       lon += x_step_size
-#     points.append(row)
-#     lat -= y_step_size
-#   elevations = []
-#   for row in points:
-#     ele_row = []
-#     for point in row:
-#       ele_row.append(elevation_data.get_elevation(point[0], point[1]))
-#     elevations.append(ele_row)
-#   print "THAR SHE BLOWS, process time: " + str(datetime.datetime.now()-intime)
+  while(lat > south):
+    if (lat <= -90): lat += 180
+    lon = west
+    row = []
+    while(lon < east):
+      if (lon >= 180): lon -=360
+      row.append([lat, lon])
+      lon += x_step_size
+    points.append(row)
+    lat -= y_step_size
+  elevations = []
+  for row in points:
+    ele_row = []
+    for point in row:
+      ele_row.append(elevation_data.get_elevation(point[0], point[1]))
+    elevations.append(ele_row)
+  print "THAR SHE BLOWS, process time: " + str(datetime.datetime.now()-intime)
   
-#   ele_str = "|".join([(",".join(str(i) for i in a)) for a in elevations])
-#   return (ele_str), 200
+  ele_str = "|".join([(",".join(str(i) for i in a)) for a in elevations])
+  return (ele_str), 200
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
